5b76179fcc25af059766b1ede54957502ed358f4
Sumit <sumitbhuia100@gmail.com>
2024-12-06 20:27:14 +0530

    callback api added

---
+++ app/api/auth/callback/0auth-callback.ts
@@ -0,0 +1,44 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+
+export async function GET(request: Request) {
+	const { searchParams, origin } = new URL(request.url);
+	const code = searchParams.get("code");
+	// const next = searchParams.get("next") ?? "/";  this was correct
+	const redirectTo = searchParams.get("redirect_to") || "/"; // this is for above
+
+console.log("Received code:", code);
+console.log("Redirecting to:", redirectTo);
+
+	if (code) {
+		const cookieStore = cookies();
+		const supabase = createServerClient(
+			process.env.NEXT_PUBLIC_SUPABASE_URL!,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			{
+				cookies: {
+					get(name: string) {
+						return cookieStore.get(name)?.value;
+					},
+					set(name: string, value: string, options: CookieOptions) {
+						cookieStore.set({ name, value, ...options });
+					},
+					remove(name: string, options: CookieOptions) {
+						cookieStore.delete({ name, ...options });
+					},
+				},
+			}
+		);
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+		if (!error) {
+			return NextResponse.redirect(`${origin}${redirectTo}`);
+		}
+		else {
+			console.error("Error exchanging code for session:", error.message);
+		  }
+	}
+
+	// return the user to an error page with instructions
+	return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
